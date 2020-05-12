06b5158f76f74f72d27cbce66664de5c76aa487c
ucef8233 <-u>
2020-05-12 00:36:33 +0000

    change compile css

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,1 +1,10 @@
-$
+$(".icona").click((e) => {
+  if ($(".nav__part3").css("display") == "none") {
+    $(".nav__part3").css("display", "flex");
+    // $(".nav__part3").css("justify-content", "space-between");
+    // $(".nav__part3").css("flex-direction", "column");
+  } else if ($(".nav__part3").css("display") == "flex") {
+    $(".nav__part3").css("display", "none");
+  }
+});
+$(".icona").css("cursor", "grab");
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
-$
+$(".icona").click((e) => {
+  if ($(".nav__part3").css("display") == "none") {
+    $(".nav__part3").css("display", "flex");
+    // $(".nav__part3").css("justify-content", "space-between");
+    // $(".nav__part3").css("flex-direction", "column");
+  } else if ($(".nav__part3").css("display") == "flex") {
+    $(".nav__part3").css("display", "none");
+  }
+});
+$(".icona").css("cursor", "grab");
+
 // BTN
 let addSection = document.getElementById('add-collapse');
 let editSection = document.getElementById('edit-collapse');
